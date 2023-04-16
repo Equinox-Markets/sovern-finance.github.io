f6f2773496c5bef41b23c01b97bdd680b78e77e1
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-16 14:53:45 -0500

    Update numerology-widget.js

--- numerology-widget.js
+++ numerology-widget.js
@@ -36,24 +36,6 @@ function calculateBalanceNumber(name) {
     return firstInitial.charCodeAt(0) - 64;
 }
 
-function calculateLuckyNumber(birthdate) {
-    const [_, day, year] = birthdate.split('/');
-    const sum = parseInt(day) + parseInt(year);
-    return reduceToSingleDigit(sum);
-}
-
-function calculateSignatureNumber(name) {
-    const letters = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/));
-    const letterValues = letters.map(c => c.charCodeAt(0) - 64);
-    const sum = letterValues.reduce((a, b) => a + b, 0);
-    return sum % 9 === 0 ? 9 : sum % 9;
-}
-
-function calculateBirthdayNumber(birthdate) {
-    const [__, day, ___] = birthdate.split('/');
-    return parseInt(day);
-}
-
 function calculatePersonalYearNumber(birthdate, currentYear) {
     const [month, day, _] = birthdate.split('/');
     const sum = parseInt(month) + parseInt(day) + parseInt(currentYear);
@@ -84,9 +66,6 @@ document.getElementById("numerology-form").addEventListener("submit", function (
     const personalityNumber = calculatePersonalityNumber(name);
     const maturityNumber = calculateMaturityNumber(lifePathNumber, lifeDestinyNumber);
     const balanceNumber = calculateBalanceNumber(name);
-    const luckyNumber = calculateLuckyNumber(birthdate);
-    const signatureNumber = calculateSignatureNumber(name);
-    const birthdayNumber = calculateBirthdayNumber(birthdate);
     const personalYearNumber = calculatePersonalYearNumber(birthdate, currentYear);
     const personalMonthNumber = calculatePersonalMonthNumber(personalYearNumber, currentMonth);
 
@@ -98,9 +77,6 @@ document.getElementById("numerology-form").addEventListener("submit", function (
         <p>Personality Number: ${personalityNumber}</p>
         <p>Maturity Number: ${maturityNumber}</p>
         <p>Balance Number: ${balanceNumber}</p>
-        <p>Lucky Number: ${luckyNumber}</p>
-        <p>Signature Number: ${signatureNumber}</p>
-        <p>Birthday Number: ${birthdayNumber}</p>
         <p>Personal Year Number: ${personalYearNumber}</p>
         <p>Personal Month Number: ${personalMonthNumber}</p>
         <p class="description"><a href="https://ethankg.wixsite.com/cosmicinsights" target="_blank">Learn More About Your Numbers Here</a></p>
