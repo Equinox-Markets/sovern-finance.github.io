1bee9bc2aefe6e751ff3e735195d5152d6f5ca9a
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-16 12:51:32 -0500

    Update numerology-widget.js

--- numerology-widget.js
+++ numerology-widget.js
@@ -39,68 +39,69 @@ document.getElementById("numerology-form").addEventListener("submit", function (
     `;
     
     document.getElementById("results").innerHTML = resultsHtml;
+    });
     
-    // Numerology functions
-    function reduceToSingleDigit(number) {
-        return number <= 9 ? number : reduceToSingleDigit(number.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b), 0));
-    }
-    
-    function calculateLifePathNumber(birthdate) {
-        const [month, day, year] = birthdate.split('/');
-        const sum = parseInt(month) + parseInt(day) + parseInt(year);
-        return reduceToSingleDigit(sum);
-    }
-    
-    function calculateLifeDestinyNumber(name) {
-        const letters = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/));
-        const letterValues = letters.map(c => c.charCodeAt(0) - 64);
-        return reduceToSingleDigit(letterValues.reduce((a, b) => a + b, 0));
-    }
-    
-    function calculateSoulNumber(name) {
-        const vowels = name.toUpperCase().split('').filter(c => 'AEIOU'.includes(c));
-        const vowelValues = vowels.map(c => c.charCodeAt(0) - 64);
-        return reduceToSingleDigit(vowelValues.reduce((a, b) => a + b, 0));
-    }
-    
-    function calculatePersonalityNumber(name) {
-        const consonants = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/) && !'AEIOU'.includes(c));
-        const consonantValues = consonants.map(c => c.charCodeAt(0) - 64);
-        return reduceToSingleDigit(consonantValues.reduce((a, b) => a + b, 0));
-    }
-    function calculateMaturityNumber(lifePathNumber, lifeDestinyNumber) {
-        return reduceToSingleDigit(lifePathNumber + lifeDestinyNumber);
-    }
-    function calculateBalanceNumber(name) {
-        const firstInitial = name[0].toUpperCase();
-        return firstInitial.charCodeAt(0) - 64;
-    }
-    
-    function calculateLuckyNumber(birthdate) {
-        const [_, day, year] = birthdate.split('/');
-        const sum = parseInt(day) + parseInt(year);
-        return reduceToSingleDigit(sum);
-    }
-    
-    function calculateSignatureNumber(name) {
-        const letters = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/));
-        const letterValues = letters.map(c => c.charCodeAt(0) - 64);
-        const sum = letterValues.reduce((a, b) => a + b, 0);
-        return sum % 9 === 0 ? 9 : sum % 9;
-    }
-    
-    function calculateBirthdayNumber(birthdate) {
-        const [_, day, _] = birthdate.split('/');
-        return parseInt(day);
-    }
-    function calculatePersonalYearNumber(birthdate, currentYear) {
-        const [month, day, _] = birthdate.split('/');
-        const sum = parseInt(month) + parseInt(day) + parseInt(currentYear);
-        return reduceToSingleDigit(sum);
-    }
-    
-    function calculatePersonalMonthNumber(personalYearNumber, currentMonth) {
-        return reduceToSingleDigit(personalYearNumber + currentMonth);
-    }
+function reduceToSingleDigit(number) {
+  return number <= 9 ? number : reduceToSingleDigit(number.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b), 0));
+}
 
-});
+function calculateLifePathNumber(birthdate) {
+  const [month, day, year] = birthdate.split('/');
+  const sum = parseInt(month) + parseInt(day) + parseInt(year);
+  return reduceToSingleDigit(sum);
+}
+
+function calculateLifeDestinyNumber(name) {
+  const letters = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/));
+  const letterValues = letters.map(c => c.charCodeAt(0) - 64);
+  return reduceToSingleDigit(letterValues.reduce((a, b) => a + b, 0));
+}
+
+function calculateSoulNumber(name) {
+  const vowels = name.toUpperCase().split('').filter(c => 'AEIOU'.includes(c));
+  const vowelValues = vowels.map(c => c.charCodeAt(0) - 64);
+  return reduceToSingleDigit(vowelValues.reduce((a, b) => a + b, 0));
+}
+
+function calculatePersonalityNumber(name) {
+  const consonants = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/) && !'AEIOU'.includes(c));
+  const consonantValues = consonants.map(c => c.charCodeAt(0) - 64);
+  return reduceToSingleDigit(consonantValues.reduce((a, b) => a + b, 0));
+}
+
+function calculateMaturityNumber(lifePathNumber, lifeDestinyNumber) {
+  return reduceToSingleDigit(lifePathNumber + lifeDestinyNumber);
+}
+
+function calculateBalanceNumber(name) {
+  const firstInitial = name[0].toUpperCase();
+  return firstInitial.charCodeAt(0) - 64;
+}
+
+function calculateLuckyNumber(birthdate) {
+  const [_, day, year] = birthdate.split('/');
+  const sum = parseInt(day) + parseInt(year);
+  return reduceToSingleDigit(sum);
+}
+
+function calculateSignatureNumber(name) {
+  const letters = name.toUpperCase().split('').filter(c => c.match(/[A-Z]/));
+  const letterValues = letters.map(c => c.charCodeAt(0) - 64);
+  const sum = letterValues.reduce((a, b) => a + b, 0);
+  return sum % 9 === 0 ? 9 : sum % 9;
+}
+
+function calculateBirthdayNumber(birthdate) {
+  const [_, day, _] = birthdate.split('/');
+  return parseInt(day);
+}
+
+function calculatePersonalYearNumber(birthdate, currentYear) {
+  const [month, day, _] = birthdate.split('/');
+  const sum = parseInt(month) + parseInt(day) + parseInt(currentYear);
+  return reduceToSingleDigit(sum);
+}
+
+function calculatePersonalMonthNumber(personalYearNumber, currentMonth) {
+  return reduceToSingleDigit(personalYearNumber + currentMonth);
+}
