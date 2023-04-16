d18b0855940670cc418677bdae1657843cdbf3d7
Ethan <112795600+Sovern-Finance@users.noreply.github.com>
2023-04-16 11:56:30 -0500

    Update numerology-widget.js

--- numerology-widget.js
+++ numerology-widget.js
@@ -11,6 +11,37 @@ document.getElementById("numerology-form").addEventListener("submit", function (
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth() + 1;
 
+    // Numerology calculations
+    const lifePathNumber = calculateLifePathNumber(birthdate);
+    const lifeDestinyNumber = calculateLifeDestinyNumber(name);
+    const soulNumber = calculateSoulNumber(name);
+    const personalityNumber = calculatePersonalityNumber(name);
+    const maturityNumber = calculateMaturityNumber(lifePathNumber, lifeDestinyNumber);
+    const balanceNumber = calculateBalanceNumber(name);
+    const luckyNumber = calculateLuckyNumber(birthdate);
+    const signatureNumber = calculateSignatureNumber(name);
+    const birthdayNumber = calculateBirthdayNumber(birthdate);
+    const personalYearNumber = calculatePersonalYearNumber(birthdate, currentYear);
+    const personalMonthNumber = calculatePersonalMonthNumber(personalYearNumber, currentMonth);
+
+    const resultsHtml = `
+        <h2>Results</h2>
+        <p>Life Path Number: ${lifePathNumber}</p>
+        <p>Life Destiny Number: ${lifeDestinyNumber}</p>
+        <p>Soul Number: ${soulNumber}</p>
+        <p>Personality Number: ${personalityNumber}</p>
+        <p>Maturity Number: ${maturityNumber}</p>
+        <p>Balance Number: ${balanceNumber}</p>
+        <p>Lucky Number: ${luckyNumber}</p>
+        <p>Signature Number: ${signatureNumber}</p>
+        <p>Birthday Number: ${birthdayNumber}</p>
+        <p>Personal Year Number: ${personalYearNumber}</p>
+        <p>Personal Month Number: ${personalMonthNumber}</p>
+    `;
+
+    document.getElementById("results").innerHTML = resultsHtml;
+});
+
     // Numerology functions
 
 function reduceToSingleDigit(number) {
